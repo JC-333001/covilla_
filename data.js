e63a03ad4e2a13c0f4467d965b744a94a19bff48
Jiaxi Chen <jiaxichen@Jiaxis-MacBook-Pro.local>
2024-09-24 18:33:51 -0700

    first commit

---
+++ data.js
@@ -0,0 +1,70 @@
+export const vacation_data = [
+  {
+    country: "EGYPT",
+    cities: [
+      {
+        name: "Bahariya Oasis",
+        img: "https://cdn.prod.website-files.com/606764630d23c37bf9d41bb1/607dd011e021d45c28b27a10_021.jpeg",
+      },
+      {
+        name: "Cairo",
+        img: "https://cdn.prod.website-files.com/606764630d23c37bf9d41bb1/607dd011e021d4d191b27a11_022.jpeg",
+      },
+      {
+        name: "Luxor",
+        img: "https://cdn.prod.website-files.com/606764630d23c37bf9d41bb1/607dd02d03615176c83d10e5_024.jpeg",
+      },
+    ],
+  },
+  {
+    country: "GREECE",
+    cities: [
+      {
+        name: "Athens",
+        img: "https://cdn.prod.website-files.com/606764630d23c37bf9d41bb1/607dcee44ed8fe667ad63e34_013.jpeg",
+      },
+      {
+        name: "Santorini",
+        img: "https://cdn.prod.website-files.com/606764630d23c37bf9d41bb1/607dceb50894ded296b2297d_019.jpeg",
+      },
+      {
+        name: "Mykonos",
+        img: "	https://cdn.prod.website-files.com/606764630d23c37bf9d41bb1/607dcd3355581e17e093caed_011.jpeg",
+      },
+    ],
+  },
+  {
+    country: "FRANCE",
+    cities: [
+      {
+        name: "Paris",
+        img: "https://cdn.prod.website-files.com/606764630d23c37bf9d41bb1/607dcd1ef1d9b0b767714525_001.jpeg",
+      },
+      {
+        name: "Lyon",
+        img: "	https://cdn.prod.website-files.com/606764630d23c37bf9d41bb1/607dcd5d6a5c000c4f1cf28e_005.jpeg",
+      },
+      {
+        name: "Marseille",
+        img: "https://cdn.prod.website-files.com/606764630d23c37bf9d41bb1/607dcd73bae3eb8d1e477694_004.jpeg",
+      },
+    ],
+  },
+  {
+    country: "INDONESIA",
+    cities: [
+      {
+        name: "Bali",
+        img: "https://cdn.prod.website-files.com/606764630d23c37bf9d41bb1/607dd099bae3eb2843495ff1_034.jpeg",
+      },
+      {
+        name: "Jakarta",
+        img: "https://cdn.prod.website-files.com/606764630d23c37bf9d41bb1/607dd099bae3eb0eaf495fc9_035.jpeg",
+      },
+      {
+        name: "Surabaya",
+        img: "	https://cdn.prod.website-files.com/606764630d23c37bf9d41bb1/607dd0b068e13e9e0a4aa1e0_036.jpeg",
+      },
+    ],
+  },
+];
